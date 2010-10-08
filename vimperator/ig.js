391637be014096e7846cad2d7aaaf1b1bb1e4531
Jason Adams <julietmikealpha@gmail.com>
2010-10-08 12:13:29 +0100

    Added vimperator plugin to scripts repository.

---
+++ vimperator/ig.js
@@ -0,0 +1,12 @@
+// Vimperator Plugin: 'ig'
+// Last Change: 26-Aug-2010 
+// License: MIT
+// Maintainer: Jason Adams <julietmikealpha@gmail.com>
+// Usage: Use :ig 
+// Usage: if successfully you'll be at your iGoogle page ;) 
+
+commands.addUserCommand(['ig'], "Navigate to your iGoogle page",
+						function(args) {
+							var url = "http://www.google.com/ig";
+							liberator.open(url, liberator.CURRENT_TAB);
+						});
