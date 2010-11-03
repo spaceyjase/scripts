73d202b6f59f48a0c0b19d68df04eed696c8bbd3
Jason Michael Adams <julietmikealpha@gmail.com>
2010-11-03 22:04:54 +0000

    OK, switch to 'libglade' because it is easier to use combo box and the code is much simplier now.  Though: could use mono instead? Just code it... ;)

--- mmml/mmml.py
+++ mmml/mmml.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python
 
 import gtk
-import pygtk
+import gtk.glade
 
 class GUI:
     def __init__(self):
-        builder = gtk.Builder()
-        builder.add_from_file("menudlg.gtk")
-        builder.connect_signals({"on_window_destroy" : gtk.main_quit,
-                                 "gtk_main_quit" : gtk.main_quit,
-                                 "on_buttonOk_clicked" : self.on_ok_clicked,
-                                 "on_buttonCancel_clicked" : self.on_cancel_clicked})
-        self.window = builder.get_object("dialog1")
-        self.window.show()
+        self.dlg = gtk.glade.XML("menudlg.glade")
+        self.dlg.signal_autoconnect(self)
 
-    def on_ok_clicked(self, widget):
+    def on_button_ok_clicked(self, widget):
         print "OK clicked!"
+        # Assuming everything is OK ;)
+        gtk.main_quit()
 
-    def on_cancel_clicked(self, widget):
+    def on_button_cancel_clicked(self, widget):
         gtk.main_quit()
 
 if __name__ == "__main__":
