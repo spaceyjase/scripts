d8c18312b47407ed11d4eb9081fa9c8ba1d0f65e
Jason Michael Adams <julietmikealpha@gmail.com>
2017-09-07 09:59:45 +0100

    Tweak the regex a bit, optional 'the'.

--- rrbot/rrbot.py
+++ rrbot/rrbot.py
@@ -8,7 +8,7 @@ reddit = praw.Reddit('rrbot')
 
 subreddit = reddit.subreddit("bodyweightfitness")
 
-what_regex = "w[h]?at('s| is) the rr|where do i find the acronyms\?"
+what_regex = "w[h]?at('s| is) (the rr|rr)|where do i find the acronyms\?"
 
 post_store = "posts_replied_to.txt"
 comment_store = "comments_replied_to.txt"
